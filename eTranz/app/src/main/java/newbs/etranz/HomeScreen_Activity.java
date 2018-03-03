b226dd539f682bd0e7a0b560cf55c461d531e1be
JValentas <j.valentas14@gmail.com>
2018-03-03 22:13:31 +0200

    HomeScreen v0.01

---
+++ eTranz/app/src/main/java/newbs/etranz/HomeScreen_Activity.java
@@ -0,0 +1,13 @@
+package newbs.etranz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeScreen_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen_);
+    }
+}
