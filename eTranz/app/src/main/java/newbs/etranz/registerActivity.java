d9e16346834282f408db656b07e7bbb14a1507bb
edvinasdi <edvinas159357@gmail.com>
2018-03-03 23:53:11 +0200

    renamed Register.java -> registerActivity.java

--- eTranz/app/src/main/java/newbs/etranz/registerActivity.java
+++ eTranz/app/src/main/java/newbs/etranz/registerActivity.java
@@ -3,7 +3,7 @@ package newbs.etranz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Register extends AppCompatActivity {
+public class registerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
