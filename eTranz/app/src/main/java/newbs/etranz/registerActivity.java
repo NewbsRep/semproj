d67021fd2186378cafed80e9dea3400fd037ac17
edvinasdi <edvinas159357@gmail.com>
2018-03-04 22:24:46 +0200

    Vėl žaidžiau su register...

--- eTranz/app/src/main/java/newbs/etranz/registerActivity.java
+++ eTranz/app/src/main/java/newbs/etranz/registerActivity.java
@@ -2,6 +2,10 @@ package newbs.etranz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class registerActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class registerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button btnRegister = (Button) findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_login);
+            }
+        });
+
+        EditText passwordInput = (EditText) findViewById(R.id.etPassword);
+        EditText repeatPasswordInput = (EditText) findViewById(R.id.etPasswordRepeat);
+        InputFilter[] filterArray = new InputFilter[1];
+        filterArray[0] = new InputFilter.LengthFilter(5);
+        passwordInput.setFilters(filterArray);
+        repeatPasswordInput.setFilters(filterArray);
+
     }
 }
