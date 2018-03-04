9e7cd4f8a0be03525ac8fc13b63521f3136543cc
edvinasdi <edvinas159357@gmail.com>
2018-03-04 23:17:22 +0200

    taisom

--- eTranz/app/src/main/java/newbs/etranz/registerActivity.java
+++ eTranz/app/src/main/java/newbs/etranz/registerActivity.java
@@ -1,5 +1,6 @@
 package newbs.etranz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputFilter;
@@ -14,20 +15,17 @@ public class registerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        Button btnRegister = (Button) findViewById(R.id.btnRegister);
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setContentView(R.layout.activity_login);
-            }
-        });
+//        EditText passwordInput = (EditText) findViewById(R.id.etPassword);
+//        EditText repeatPasswordInput = (EditText) findViewById(R.id.etPasswordRepeat);
+//        InputFilter[] filterArray = new InputFilter[1];
+//        filterArray[0] = new InputFilter.LengthFilter(5);
+//        passwordInput.setFilters(filterArray);
+//        repeatPasswordInput.setFilters(filterArray);
 
-        EditText passwordInput = (EditText) findViewById(R.id.etPassword);
-        EditText repeatPasswordInput = (EditText) findViewById(R.id.etPasswordRepeat);
-        InputFilter[] filterArray = new InputFilter[1];
-        filterArray[0] = new InputFilter.LengthFilter(5);
-        passwordInput.setFilters(filterArray);
-        repeatPasswordInput.setFilters(filterArray);
+    }
 
+    public void registerButtonPressed(View view){
+        Intent intent = new Intent(this, Login_Activity.class);
+        startActivity(intent);
     }
 }
