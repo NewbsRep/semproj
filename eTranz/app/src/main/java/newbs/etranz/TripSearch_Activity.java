e41bf745dbb6196f567e38d6dc7a8eb2ca8facfa
Vismantas Abromavičius <vabrom@gmail.com>
2018-04-08 16:14:31 +0300

    Updated new trip layout with date/time pickers, buttons to inc/dec price and seats and dropdown menus for locations.

--- eTranz/app/src/main/java/newbs/etranz/TripSearch_Activity.java
+++ eTranz/app/src/main/java/newbs/etranz/TripSearch_Activity.java
@@ -3,11 +3,11 @@ package newbs.etranz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SearchForATrip_Activity extends AppCompatActivity {
+public class TripSearch_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_for_a_trip);
+        setContentView(R.layout.activity_trip_search);
     }
 }
